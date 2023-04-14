ad4ce9af9755a24f90e8aa35111b8956721999bd
the-sides <jacob.r.sides@gmail.com>
2023-04-14 12:57:12 -0400

    Test some data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,39 @@
 <script>
-	export let data;
+	import { onMount } from 'svelte';
 
-	// But first, convert to TS
-	// // Store
-	// const accessToken = data.token;
-	// // const token = writable()
-	// localStorage.setItem('access_token', accessToken);
+	export let data;
+	let dumpElm;
+	let token;
+	onMount(() => {
+		// Clear url params
+		// TODO adjust param object directly, better way to write probably
+		window.history.replaceState('HomePage', 'Music Kit', '/');
+		if (data.tokenObj !== null) {
+			window.localStorage.setItem('token', JSON.stringify(data.tokenObj));
+		}
+		token = JSON.parse(window.localStorage.getItem('token') ?? '{}').access_token;
+		const tracks = fetch('https://api.spotify.com/v1/me/playlists?limit=50', {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		})
+			.then((rs) => rs.json())
+			.then((data) => {
+				dumpElm.innerHTML = JSON.stringify(data.items.map((item) => item.name));
+			})
+			.catch(console.error);
+	});
 </script>
 
 <div class="absolute top-8 right-8 flex gap-8">
 	<p>
-		Token: <span class="border-b border-black px-2">
-			{data.token?.access_token?.substring(0, 8)}</span
-		>
+		Token: <span class="border-b border-black px-2"> {(token ?? '').substring(0, 8)}</span>
 	</p>
 	<a class="" href="/login">Login</a>
 </div>
 
-<div class="grid h-screen place-content-center text-8xl">
-	<h1>Music Mind</h1>
+<div class="grid min-h-screen place-content-center px-4 mx-auto">
+	<h1 class="text-8xl">Music Kit</h1>
 	<h2 class="text-4xl my-4">The Spotify app to rule them all.</h2>
+	<p bind:this={dumpElm} class="tracks overflow-auto max-w-[700px]" />
 </div>
